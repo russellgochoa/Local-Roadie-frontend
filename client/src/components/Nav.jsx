da4ba1b53d4103b6dc2a91198873bababe84c53a
Russell Ochoa <ochoa.russell@gmail.com>
2022-09-09 14:27:04 -0500

    create basic about page

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,25 @@
 import { NavLink } from 'react-router-dom'
 
 
-const publicOptions = (
-  <div>
-    <NavLink to="/about" className='nav-link about'>About</NavLink>
-    <NavLink to="/login" className='nav-link login'>Login</NavLink>
-  </div>
-)
+const Nav = () => {
+  const publicOptions = (
+      <div>
+        <NavLink to="/about" className='nav-link about'>About</NavLink>
+        <NavLink to="/login" className='nav-link login'>Login</NavLink>
+      </div>
+    );
 
+
+
+return (
+  <nav>
+    <NavLink to="/" className="nav-logo-link" >
+      <div className="logo-wrapper" alt="logo">
+        <img className="logo"/>
+        <h1 className='nav-title'>Local Roadie</h1>
+      </div>
+      </NavLink>
+      </nav>
+    )
+    }
 export default Nav
---
+++ client/src/pages/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="about-section">
+      <h1>How does Local Roadie work?</h1>
+      <p className="about-blurb"></p>
+    </div>
+  )
+}
+export default About
