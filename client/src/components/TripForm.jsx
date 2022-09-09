349671819b9cd7465f664daf9c13f3ec032cbb2a
Russell Ochoa <ochoa.russell@gmail.com>
2022-09-09 16:22:03 -0500

    populate UpdateTripForm

--- client/src/components/TripForm.jsx
+++ client/src/components/TripForm.jsx
@@ -6,18 +6,18 @@ const TripForm = ({user}) => {
   <h1 className='trip-form-title'>Book a Trip</h1>
     <form onSubmit={handleSubmit}>
       <label htmlFor="date">Date:</label>
-      <input type="string" id="date" onChange={handleChange} />
+      <input type="string" id="date" name="date" onChange={handleChange} />
       <p></p>
       <label htmlFor="pick-up-time">Pick Up Time:</label>
-      <input type="string" id="pick-up-time" onChange={handleChange} />
+      <input type="string" id="pick-up-time" name="pick-up-time" onChange={handleChange} />
       <p></p>
       <label htmlFor="pick-up-location">Pick Up Location:</label>
-      <input type="string" id="pick-up-location" onChange={handleChange} />
+      <input type="string" id="pick-up-location" name="pick-up-location" onChange={handleChange} />
       <p></p>
       <label htmlFor="destination">Destination:</label>
-      <input type="string" id="destination" onChange={handleChange} />
+      <input type="string" id="destination" name="destination" onChange={handleChange} />
       <label htmlFor="gear">Gear:</label>
-      <input type="text" id="gear" onChange={handleChange} />
+      <input type="text" id="gear" name="gear" onChange={handleChange} />
       <p></p>
       <button type="submit">Update Trip</button>
       <p></p>
--- client/src/components/UpdateTripForm.jsx
+++ client/src/components/UpdateTripForm.jsx
@@ -1,5 +1,110 @@
-import { useNavigate } from "react-router"
 import { useState, useEffect } from "react"
 import { UpdateUser } from "../services/Authorize"
 import axios from "axios"
 import { BASE_URL } from "../globals"
+
+const UpdateTripForm = ({user}) => {
+
+
+const initialState = {
+  date: user.date,
+  pickupTime: user.pickupTime,
+  pickupLocation: user.pickupLocation,
+  destination: user.destination,
+  gear: user.gear,
+  }
+
+
+const [formValues, setFormValues] = useState(initialState)
+
+const handleChange = event => {
+  setFormValues({...initialState, [event.target.name]: event.target.value})
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault()
+  UpdateUser({
+    id: user.id,
+    ...formValues
+  })
+  navigate('/')
+  window.location.reload()
+
+}
+
+return user ? (
+  <div className="trip-form-container">
+    <form className="trip-form form" onSubmit={handleSubmit}>
+      <div className="grid">
+        <div className="date">
+          <div className="date-label-container"><label htmlFor='date'>Date</label></div>
+          <input
+          onChange={handleChange}
+          date="date"
+          type="string"
+          placeholder="Date"
+          value={formValues.date}
+          required
+          />
+        </div>
+        <div className="pickupTime">
+          <div className="pickup-time-label-container"><label htmlFor='pickupTime'>Pick Up Time</label></div>
+          <input
+          onChange={handleChange}
+          date="pickupTime"
+          type="string"
+          placeholder="Pick Up Time"
+          value={formValues.pickupTime}
+          required
+          />
+        </div>
+        <div className="pickupLocation">
+          <div className="pickup-location-label-container"><label htmlFor='pickupLocation'>Pick Up Time</label></div>
+          <input
+          onChange={handleChange}
+          date="pickupLocation"
+          type="string"
+          placeholder="Pick Up Location"
+          value={formValues.pickupLocation}
+          required
+          />
+        </div>
+        <div className="destination">
+          <div className="destination-label-container"><label htmlFor='destination'>Destination</label></div>
+          <input
+          onChange={handleChange}
+          date="destination"
+          type="string"
+          placeholder="Destination"
+          value={formValues.destination}
+          required
+          />
+        </div>
+        <div className="gear">
+          <div className="gear-label-container"><label htmlFor='gear'>Gear</label></div>
+          <input
+          onChange={handleChange}
+          date="gear"
+          type="string"
+          placeholder="Gear"
+          value={formValues.gear}
+          required
+          />
+        </div>
+
+
+      </div>
+      <div className="submit-button-container">
+        <button className="submit-button button"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
+) : null
+
+
+}
+
+export default UpdateTripForm
